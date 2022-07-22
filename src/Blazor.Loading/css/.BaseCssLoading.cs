ad448dac2b3bf69143b5000c28c344b107b2ab1b
msynk <msynk@outlook.com>
2022-07-22 15:52:40 +0430

    refactor & add more loading

---
+++ src/Blazor.Loading/css/.BaseCssLoading.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Loading;
+
+public class BaseCssLoading : ComponentBase
+{
+    [Parameter] public int Size { get; set; } = 64;
+
+    [Parameter] public string Color { get; set; } = "#FFF";
+
+    protected virtual int OriginalSize { get; set; } = 80;
+    protected int Convert(double value)
+    {
+        return (int)(value / OriginalSize * Size);
+    }
+}
---
+++ src/Blazor.Loading/svg/.BaseSvgLoading.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Loading;
+
+public class BaseSvgLoading : ComponentBase
+{
+    [Parameter] public int Size { get; set; } = 200;
+}
